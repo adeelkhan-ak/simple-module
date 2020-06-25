759f1b201b5e2ccb1487b9155765d8818acd5e88
adeelkhan-ak <ak140175@gmail.com>
2020-06-25 04:45:03 -0700

    remove sys

--- arithmetic.py
+++ arithmetic.py
@@ -1,3 +1,4 @@
+import sys
 def add(x,y):
     return x+y
 def sub(x,y):
@@ -6,3 +7,17 @@ def mul(x,y):
     return x*y
 def div(x,y):
     return x/y
+if __name__ == "__main__":
+    v= str(input())
+    valOne=int (input())
+    valTwo=int(input ())
+    if v == "a":
+        print(add(valOne, valTwo))
+    elif v == "d":
+        print(div(valOne, valTwo))
+    elif v == "m":
+        print(mul(valOne, valTwo))
+    elif v == "s":
+        print(sub(valOne, valTwo))
+    else:
+        pass
